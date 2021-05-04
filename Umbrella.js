26b89eb82f4a918651aa860d36a8708bd2000e03
felix785cw <76728294+felix785cw@users.noreply.github.com>
2021-05-04 09:46:34 +0530

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -14,6 +14,8 @@ class Umbrella {
      display(){
           var pos = this.body.position;
           image(man_image, pos.x-90, pos.y-20,175,175);
+             if(frameCount >= 200)
+                   image(this.batmanImg,pos.x,pos.y+70,200,300)
 
      }
 }
